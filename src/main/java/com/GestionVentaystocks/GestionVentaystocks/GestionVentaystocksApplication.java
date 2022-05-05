275f224bc8fbe1ce963e45dbe419f825afbc7001
Facundo Monzon <fimonzon@mail.austral.edu.ar>
2022-05-05 08:30:04 -0300

    primer commit

---
+++ src/main/java/com/GestionVentaystocks/GestionVentaystocks/GestionVentaystocksApplication.java
@@ -0,0 +1,13 @@
+package com.GestionVentaystocks.GestionVentaystocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVentaystocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVentaystocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestionVentaystocks/GestionVentaystocks/GestionVentaystocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionVentaystocks.GestionVentaystocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVentaystocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
